764cebb255226e84f66db172ea53bc4577f70132
Gabriela Mendes <gabmdoss@gmail.com>
2024-02-08 14:34:38 -0300

    Login scenarios

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# front-end-cypress
+## front-end-cypress
 Front End cypress automation using cucumber
+
+## Init project
+
+npm -i init
+
+## Install Cypress
+
+npm install cypress
+
+## Open Cypress
+
+npx cypress open
+
+## Cypress terminal
+
+npx cypress run
+
+
---
+++ cypress/e2e/login.js
@@ -0,0 +1,63 @@
+describe('Login', () => {
+
+  beforeEach(() => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+  })
+
+  it('Login successfully', () => {
+    cy.get('[name="username"]')
+      .should('be.visible')
+      .type('Admin')
+    cy.get('[name="password"]')
+      .should('be.visible')
+      .type('admin123')
+    cy.get('.oxd-button').click()
+    cy.url()
+      .should('be.equal', 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb')
+      .should('be.visible')
+  })
+
+  it('Password Incorrect', () => {
+    cy.get('[name="username"]')
+      .should('be.visible')
+      .type('Admin')
+    cy.get('[name="password"]')
+      .should('be.visible')
+      .type('admin321')
+    cy.get('.oxd-button').click()
+    cy.contains('Invalid credentials').should('be.visible')
+  })
+
+  it('Username Incorrect', () => {
+    cy.get('[name="username"]')
+      .should('be.visible')
+      .type('Adm')
+    cy.get('[name="password"]')
+      .should('be.visible')
+      .type('admin123')
+    cy.get('.oxd-button').click()
+    cy.contains('Invalid credentials').should('be.visible')
+  })
+
+  it('Password empty', () => {
+    cy.get('[name="username"]')
+      .should('be.visible')
+      .type('Admin')
+    cy.get('.oxd-button').click()
+    cy.contains('Required').should('be.visible')
+  })
+
+  it('Username empty', () => {
+    cy.get('[name="password"]')
+      .should('be.visible')
+      .type('admin123')
+    cy.get('.oxd-button').click()
+    cy.contains('Required').should('be.visible')
+  })
+
+  
+  it('Username and password empty', () => {
+    cy.get('.oxd-button').click()
+    cy.contains('Required').should('be.visible') })
+});
