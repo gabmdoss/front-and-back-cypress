617e9b3798c665eb9426f8641b1115c97fcf74b4
Gabriela Mendes <gabmdoss@gmail.com>
2025-03-26 22:16:07 -0300

    feature/B01: Allure reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,16 @@
 const { defineConfig } = require("cypress");
-
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://opensource-demo.orangehrmlive.com",
-  },
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
+    env: {
+      allure: true,
+      allureAddVideoOnPass: true
+    }
+  }
 });
